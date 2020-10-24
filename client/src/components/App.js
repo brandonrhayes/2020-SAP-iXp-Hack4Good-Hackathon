fdb51ca901b50bbad2fa029d3fe2f026a8da8119
mandywan <mandygwan@gmail.com>
2020-10-23 23:02:57 -0700

    merge mandy and helen

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,56 +1,14 @@
 import React from 'react';
 import './App.css';
-<<<<<<< HEAD
-import FacebookLogin from 'react-facebook-login';
-import axios from 'axios';
-
-=======
 import Header from './Header';
 import Home from './Home';
 import Account from './Account';
 import Logger from './Logger';
 // import { PhotoCaptureUSB } from './photo/PhotoCaptureUSB';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
->>>>>>> mandy
 
 function App() {
-  // return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <h1>let's start hacking!</h1>
-  //     </header>
-  //   </div>
-  // );
-
-  const responseFacebook = (response) => {
-    console.log(response);
-    axios({
-      method: "POST",
-      url: "http://localhost:9000/facebooklogin",
-      data: {
-        name: response.name,
-        email: response.email
-      }
-    }).then(response => {
-      console.log("Facebook login success", response);
-    })
-  }
-
   return (
-<<<<<<< HEAD
-    <div className = "facebook login">
-      <h1>Login with Facebook</h1>
-      <FacebookLogin
-        appId="661278961420678"
-        autoLoad={true}
-        fields="name,email,picture"
-        callback={responseFacebook}
-        cssClass="my-facebook-button-class"
-        icon="fa-facebook"
-      />,
-    </div>
-  )
-=======
     <Router>
       <div>
         <Header />
@@ -65,7 +23,6 @@ function App() {
       </div>
     </Router>
   );
->>>>>>> mandy
 }
 
 export default App;
--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,8 +1,16 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { AppBar, Toolbar, Typography, Button, IconButton } from "@material-ui/core";
-import { AccountCircle } from '@material-ui/icons';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  IconButton,
+} from "@material-ui/core";
+import { AccountCircle } from "@material-ui/icons";
 import { Link } from "react-router-dom";
+import FacebookLogin from "react-facebook-login";
+import axios from "axios";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -12,13 +20,27 @@ const useStyles = makeStyles((theme) => ({
     flexGrow: 1,
   },
   link: {
-    textDecoration: 'none',
-    color: 'white',
-  }
+    textDecoration: "none",
+    color: "white",
+  },
 }));
 
 const Header = () => {
   const classes = useStyles();
+
+  const responseFacebook = (response) => {
+    console.log(response);
+    axios({
+      method: "POST",
+      url: "http://localhost:9000/facebooklogin",
+      data: {
+        name: response.name,
+        email: response.email,
+      },
+    }).then((response) => {
+      console.log("Facebook login success", response);
+    });
+  };
 
   return (
     <div className={classes.root}>
@@ -27,19 +49,25 @@ const Header = () => {
           <Typography variant="h6" className={classes.title}>
             Mindr
           </Typography>
-          <Link to={'/'} className={classes.link}>
+          <Link to={"/"} className={classes.link}>
             <Button color="inherit">Home</Button>
           </Link>
-          <Link to={'/log'} className={classes.link}>
+          <Link to={"/log"} className={classes.link}>
             <Button color="inherit">Log</Button>
           </Link>
-          <Link to={'/account'} className={classes.link}>
-            <IconButton
-              color="inherit"
-            >
+          <Link to={"/account"} className={classes.link}>
+            <IconButton color="inherit">
               <AccountCircle />
             </IconButton>
           </Link>
+          <FacebookLogin
+            appId="661278961420678"
+            autoLoad={true}
+            fields="name,email,picture"
+            callback={responseFacebook}
+            cssClass="my-facebook-button-class"
+            icon="fa-facebook"
+          />
         </Toolbar>
       </AppBar>
     </div>
